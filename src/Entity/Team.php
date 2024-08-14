b832e5556078c85ac48a2133c9b101a234580bb5
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-14 02:28:56 +0200

    Team Entity

---
+++ src/Entity/Team.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $yearOfCreation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $president = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $coach = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $logo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getYearOfCreation(): ?int
+    {
+        return $this->yearOfCreation;
+    }
+
+    public function setYearOfCreation(?int $yearOfCreation): static
+    {
+        $this->yearOfCreation = $yearOfCreation;
+
+        return $this;
+    }
+
+    public function getPresident(): ?string
+    {
+        return $this->president;
+    }
+
+    public function setPresident(string $president): static
+    {
+        $this->president = $president;
+
+        return $this;
+    }
+
+    public function getCoach(): ?string
+    {
+        return $this->coach;
+    }
+
+    public function setCoach(string $coach): static
+    {
+        $this->coach = $coach;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): static
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+}
